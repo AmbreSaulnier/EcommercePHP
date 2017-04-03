d7b14a222cde122fe78373c4a867d62a74d4cd00
Matthieu Mota <matthieumota@gmail.com>
2017-04-03 16:24:58 +0200

    WIP login

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include 'config.php';
+include 'header.php';
+
+if (1 == 1) {
+    $_SESSION['user'] = 1;
+    setcookie('cookie', 1, time() + 3600);
+}
+
+?>
+
+<?php if (isset($_SESSION['user'])) {
+    echo "Bienvenue " . $_SESSION['user'];
+} ?>
+
+<div class="container">
+    <form action="">
+    
+    </form>
+</div>
+
+<?php include 'footer.php'; ?>
