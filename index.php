0643a471e5ff7f92f26090df0809f067774dca0c
Matthieu Mota <matthieumota@gmail.com>
2017-04-03 09:37:38 +0200

    Product list

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ include 'header.php'; ?>
 
     <div class="container">
         <h1>J'ai fait la Home page !</h1>
+        <?php foreach(getProducts() as $product) {
+            var_dump($product);
+        } ?>
     </div>
 
 <?php include 'footer.php'; ?>
