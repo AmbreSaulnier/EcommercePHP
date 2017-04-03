a06ef45f03beaa166080959bad3af274a2d3174d
Matthieu Mota <matthieumota@gmail.com>
2017-04-03 10:36:19 +0200

    Add composer

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ include 'header.php'; ?>
 
     <div class="container">
         <h1>J'ai fait la Home page !</h1>
+        <?php
+            $faker = Faker\Factory::create(); // equivaut à new Faker() ou new PDO()
+            for ($i = 0; $i < 10; $i++) {
+                echo $faker->address . '<br />';
+            }
+        ?>
         <?php foreach(getProducts() as $product) {
             var_dump($product);
         } ?>
