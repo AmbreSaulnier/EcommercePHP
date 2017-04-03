6b917313faafb5925aa7a07fedd56d32f35371f1
Matthieu Mota <matthieumota@gmail.com>
2017-04-03 15:01:01 +0200

    Register function

--- functions.php
+++ functions.php
@@ -5,3 +5,23 @@ function getProducts() {
     $query = $db->query('SELECT * FROM product');
     return $query->fetchAll();
 }
+
+function userExists($email, $login) {
+    global $db;
+    // On fait une requête pour vérifier si l'utilisateur existe ou pas
+    $query = $db->prepare('SELECT COUNT(*) FROM user WHERE email = :email OR login = :login');
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
+    $query->bindValue(':login', $login, PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetchColumn(); // Return 0, 1 ou 2, fetchColumn retourne la valeur de la première colonne
+}
+
+function registerUser($login, $email, $password) {
+    $query = $db->prepare("INSERT INTO user (login, email, password, role, registered_at) VALUES (:login, :email, :password, 'customer', NOW())");
+    $query->bindValue(':login', $login);
+    $query->bindValue(':email', $email);
+    $query->bindValue(':password', password_hash($password, PASSWORD_DEFAULT));
+    if ($query->execute()) {
+        return $db->lastInsertId();
+    }
+}
---
+++ register.php
@@ -0,0 +1,61 @@
+<?php
+include 'config.php';
+include 'header.php';
+
+// J'éxecute le code si le formulaire est posté
+if (!empty($_POST)) {
+    // Je récupére les valeurs de mes champs
+    $login = $_POST['login'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Je crée un tableau d'erreur vide
+    $errors = [];
+
+    if (empty($login)) {
+        $errors['login'] = "Le login est vide.";
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "L'email n'est pas valide.";
+    }
+
+    if (strlen($password) < 8) {
+        $errors['password'] = "Le mot de passe est vide.";
+    }
+
+    if (empty($errors)) {
+        if (!userExists($email, $login)) {
+            // J'inscris mon utilisateur (functions.php)
+            $user = registerUser($login, $email, $password);
+            if ($user) {
+                echo "L'utilisateur a bien été inscrit";
+            }
+        } else {
+            echo "L'utilisateur existe déjà.";
+        }
+    }
+
+}
+
+?>
+
+    <div class="container">
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="login">Login : </label>
+                <input type="text" name="login" id="login" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email : </label>
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+            <button class="btn btn-primary">S'inscrire</button>
+        </form>
+    </div>
+
+<?php include 'footer.php'; ?>
